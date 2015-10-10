aa2270f613e8dfbfb72dab0c7d8b96390e86ae73
Arjen Heidinga <dexter@beetjevreemd.nl>
2015-10-10 17:54:50 +0200

    Max advert length/width

--- opt/panjerd/adverts.sh
+++ opt/panjerd/adverts.sh
@@ -3,6 +3,9 @@
 source /opt/panjerd/VARS.sh
 
 
+# Jaja... De credits header is iig: ICAbWzA7MzRtG1s0Mm0gLSA9ICBDIFIgRSBEIEkgVCBTICA9IC0gG1swbQoK 
+CREDITS="ICAbWzA7MzRtG1s0Mm0gLSA9ICBDIFIgRSBEIEkgVCBTICA9IC0gG1swbQoKICAgICAgICBQIEEgTiBKIEUgUiBECiBJcyBwcm91ZGx5IHByZXNlbnRlZCB0byB5b3UKICAgICBieSB0aGUgUEFOSkVSRCB0ZWFtCiwtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0uCnxCb3NzIFcgIC0gQ3B0IENhdGVyaW5nICAgICB8CnxKb2JhbWJpIC0gTGUgUGV0aXQgQ2FwaXRhaW58CnxNci4gTWFuIC0gSGFubmliYWwgICAgICAgICB8CmAtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0nCg=="
+
 #    DIT IS DE BREEDTE    #
 
 while `true`; do
@@ -11,8 +14,11 @@ while `true`; do
 	
 	FILE=$(find "${ADVERTDIR}" -iregex ".*\(txt\)" | sort -R | head -1)
 	
-	if [ -e "${FILE}" ]; then
-		cat "$FILE"
+	## Hardcoded chance on displaying the credits
+	if [ $((RANDOM%20+1)) -eq 1 ]; then
+		echo ${CREDITS} | base64 -d
+	elif [ -e "${FILE}" ]; then
+		cat "$FILE" | head -9 | cut -c 29
 	else
 		echo "Running free!"
 	fi
