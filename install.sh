1b9c81a75965aaba6608ca451e9ffb1cb693142f
Arjen Heidinga <dexter@beetjevreemd.nl>
2015-10-10 17:54:30 +0200

    install packages when installing.

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ git pull
 cp -rf opt/ /
 cp -rf home/ /
 cp -rf etc/ /
+#cp -rf lib/ /
+
+apt-get update
+apt-get install hostapd dnsmasq apache2 firmware-realtek wireless-tools wpasupplicant exfat-utils exfat-fuse ntfs-3g autofs5
