9318d3a48edf45622e689031a96674fc6d562562
Arjen Heidinga <dexter@beetjevreemd.nl>
2015-10-11 14:03:57 +0200

    Fix the credits showing.

--- opt/panjerd/adverts.sh
+++ opt/panjerd/adverts.sh
@@ -10,18 +10,21 @@ CREDITS="ICAbWzA7MzRtG1s0Mm0gLSA9ICBDIFIgRSBEIEkgVCBTICA9IC0gG1swbQoKICAgICAgICB
 
 while `true`; do
 	clear
-	echo -e "        ${BLUE}${ON_RED}Advertisement${RESET}\n"
-	
-	FILE=$(find "${ADVERTDIR}" -iregex ".*\(txt\)" | sort -R | head -1)
 	
 	## Hardcoded chance on displaying the credits
 	if [ $((RANDOM%20+1)) -eq 1 ]; then
-		echo ${CREDITS} | base64 -d
-	elif [ -e "${FILE}" ]; then
-		# The AWK cuts of the last newline
-		cat "$FILE" | head -8 | cut -c 1-29 | awk '{q=p;p=$0}NR>1{print q}END{ORS = ""; print p}'
+		echo ${CREDITS} | base64 -d | awk '{q=p;p=$0}NR>1{print q}END{ORS = ""; print p}'
 	else
-		echo "Running free!"
+		echo -e "        ${BLUE}${ON_RED}Advertisement${RESET}\n"
+		
+		FILE=$(find "${ADVERTDIR}" -iregex ".*\(txt\)" | sort -R | head -1)
+		
+		if [ -e "${FILE}" ]; then
+			# The AWK cuts of the last newline
+			cat "$FILE" | head -8 | cut -c 1-29 | awk '{q=p;p=$0}NR>1{print q}END{ORS = ""; print p}'
+		else
+			echo "Running free!"
+		fi
 	fi
 	
 	sleep $WAITTIME_ADVERT
